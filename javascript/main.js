6bd4785f97cc421e6e50aa6cd1df7ea5767b0c04
fabiooliveiradev <fabionetnews@gmail.com>
2022-07-07 23:35:27 -0300

    Finalização do layout e inicio da lógica

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,14 @@
-const OPTION_CALC = document.querySelector('#optCalc');
 
-function selectCalc(){
-        const PR_VENDA = 'prVenda';
-        const PR_CUSTO = 'prCusto';
-        const MARG_LUCRO = 'margLucro'
-        let index = OPTION_CALC.selectedIndex;
-        let choose = OPTION_CALC[index].value;
-        
-        if(choose === PR_VENDA){
-                document.querySelector('#prCompra').checked = true;
-                document.querySelector('#margLucro').checked = true;
-                document.querySelector('#inpPrCompra').value = 0,00;
-                document.querySelector('#inpMargLucro').value = 0,00;
-        }
-        console.log(choose)
-        
+const OPCAO = document.querySelector('#optCalc');
+
+OPCAO.addEventListener('change', verificaOperacao);
+
+function verificaOperacao(){
+	var operacaoEscolhida;
+	for(let a of OPCAO){
+		if(a.isChecked){
+			console.log(a)
+		}
+	}
+	
 }
